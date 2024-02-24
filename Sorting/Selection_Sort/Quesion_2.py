a2f43922e7534b8793a04666af761e1c554d239d
Saurav Chaudhari <srchaudhari0806@gmail.com>
2024-02-24 10:47:30 +0530

    Added Sort Search Practice Programs

--- Sorting/Selection_Sort/Quesion_2.py
+++ Sorting/Selection_Sort/Quesion_2.py
@@ -1,6 +1,6 @@
 # Quesion - 2
-# find_Duplicate Value in array using selection sort
-def findDuplicate(arr):
+# find_most of time Reapeated Value in array using selection sort
+def findRepeated(arr):
     n=len(arr)
     for i in range(n-1):
         mini=i
@@ -9,13 +9,12 @@ def findDuplicate(arr):
                 mini =j
                 arr[i],arr[mini]=arr[mini],arr[i]
 def minisum(arr):
-        findDuplicate(arr)
+        findRepeated(arr)
         duplicate=set()
         for i in range(1,len(arr)):
             if arr[i] == arr[i-1]:
                 duplicate.add(arr[i])
                 return duplicate
-      
-    
-arr=[22,22,55,55]
+       
+arr=[0,1,2,5646,5,646,6,16,549,6,59599,9,99,9,9,9,96,69,6,596,9,65,965,965,965,]
 print("Sorted Array:",minisum(arr))
